f351f4bfb9244903074d9cad1dc6f28ef2555de7
Mortl <122238425+Cosmo697@users.noreply.github.com>
2025-06-25 15:03:36 -0700

    Add package init files and update tests

--- tests/test_audio_device.py
+++ tests/test_audio_device.py
@@ -1,5 +1,10 @@
 import unittest
 from unittest.mock import patch
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from utils import audio_device
 
 class TestAudioDevice(unittest.TestCase):
